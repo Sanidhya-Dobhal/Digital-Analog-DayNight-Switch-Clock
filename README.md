074cfaf3ea4e03d4d617aee794f04d14e2a2444b
Sanidhya Dobhal <sanidhya.dobhal.getsuperbrain.app>
2024-09-08 17:53:01 +0530

    Refactor: abided to the standard naming convention and code formatting practices. Made a few optimization and improved code quality. Updated Readme.md as well

--- README.md
+++ README.md
@@ -11,18 +11,34 @@ A front-end clock project that showcases both digital and analog representations
   Represented by hour, minute, and second hands over a clock face.
 - <b><u>Inter-Convertibility</u>:</b> <br> Easily switch between digital and analog clock views with a click of a button. -<b><u>Customization:</u> You can customize the clock your way by selecting the color in which time would be displayed</b>
 - <b><u>Automatic Day-to-Night</u>:</b> <br>The clock theme changes to a darker, more eye-friendly version from 9 p.m. to 5 a.m.
+- <b><u>Customizable Time olor</u>:</b> User can select the color from the colr picker in which the time should be displayed. It changes the digital time and also changes the color of the numbers in the analog clock
 - <b><u>Date(dd/mm/yyyy)</u>:</b>
   Date is updated automatically at 12a.m.
 - <b><u>User-Friendly</u>:</b> <br>
-  Simple and intuitive interface for easy interaction.
+  Simple and intuitive interface for easy interaction. -<b><u>Fully Responsive</u>:</b>
+  The webpage will always fit gracefully inside the browser screen, no matter what the size of browser screen is.
+  <br>
+  Screen size as small as 180px will also show the webpage perfectly <br>
 
 ## Screenshots :
 
-<img src = "Screenshots/Screenshot 2023-07-29 at 6.00.20 AM.png">
-<img src = "Screenshots/Screenshot 2023-07-29 at 6.00.25 AM.png">
-<img src = "Screenshots/Screenshot 2023-07-29 at 6.15.30 AM.png">
-<img src = "Screenshots/Screenshot 2023-07-29 at 6.15.46 AM.png">
+### Day time interface
+
+<img src = "Screenshots/dayDigital.png">
+<img src = "Screenshots/dayAnalog.png">
+
+### Night time interface
+
+<img src = "Screenshots/nightDigital.png">
+<img src = "Screenshots/nightAnalog.png">
+
+### Changing time color
+
+<img src = Screenshots/timeColorDigital.png>
+<img src = Screenshots/timeColorAnalog.png>
+
 ## Technologies Used :
+
 This project has been created using <b>HTML,vanilla CSS ,vanilla javascript</b> .
 
 ## Installation and Usage :
--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-let analogTime = document.getElementById("Time");
+let analogTime = document.getElementById("time");
 let AMOrPM = document.getElementById("meri");
-let curr_date = document.getElementById("date");
+let dateElement = document.getElementById("date");
 let digclk = document.getElementById("digop");
 let anaclk = document.getElementById("anaop");
-let ana_clk_shp = document.getElementById("clk_shp_ana");
+let analogClock = document.getElementById("analog-clock");
 let secondsNeedle = document.getElementById("ns");
 let minutesNeedle = document.getElementById("nm");
 let hoursNeedle = document.getElementById("nh");
-let ana_func; //Stores the id of the setInterval function that calls the function func
-digclk.checked = true; //Initially digital would be selected
+let analogSetIntervalId; //Stores the id of the setInterval function that calls the function func
 analogNumbersRotation();
 setDigital();
 windowWidthHandler();
-dig_cont = document.getElementById("dig_cont");
-ana_cont = document.getElementById("ana_cont");
-dig_cont.addEventListener("click", setDigital);
+digitalRadioContainer = document.querySelector(
+  ".radio-buttons-container:nth-child(1)"
+);
+analogRadioContainer = document.querySelector(
+  ".radio-buttons-container:nth-child(2)"
+);
+digitalRadioContainer.addEventListener("click", setDigital);
 function analogNumbersRotation() {
   //This function is used to rotate all the numbers in analog clock which was earlier done in CSS one by one
   for (i = 1; i <= 12; i++) {
@@ -28,85 +31,63 @@ function analogNumbersRotation() {
 }
 function setDigital() {
   digclk.checked = true;
-  clearInterval(ana_func);
-  ana_clk_shp.style.visibility = "hidden";
-  secondsNeedle.style.visibility = "hidden";
-  minutesNeedle.style.visibility = "hidden";
-  hoursNeedle.style.visibility = "hidden";
+  clearInterval(analogSetIntervalId);
+  analogClock.style.display = "none";
   setInterval(setDigitalTime, 100);
 }
-ana_cont.addEventListener("click", setAnalog);
+analogRadioContainer.addEventListener("click", setAnalog);
 function setAnalog() {
   anaclk.checked = true;
-  ana_clk_shp.style.visibility = "visible";
-  clearInterval(ana_func); //This is required as without this if the analog option is selected twice the there would be 2 setinterval's runnig and then if we select digital then only the alstest setInterval function will be stopped but the others will keep getting executed .Therefore this command will stop all the previous sstinterval's and inly the one that would be in the ana_func variable would be running .
-  ana_func = setInterval(setAnalogTime, 100);
+  clearInterval(analogSetIntervalId); //This is required as without this if the analog option is selected twice the there would be 2 setinterval's runnig and then if we select digital then only the alstest setInterval function will be stopped but the others will keep getting executed .Therefore this command will stop all the previous sstinterval's and inly the one that would be in the analogSetIntervalId variable would be running .
+  analogSetIntervalId = setInterval(setAnalogTime, 100);
 }
 window.addEventListener("resize", windowWidthHandler);
 function windowWidthHandler() {
   let leftSpace;
   if (window.innerWidth > 850) leftSpace = "40vw";
   else leftSpace = `${(window.innerWidth - 170) / 2}px`;
-  document.getElementById("clk_shp_ana").style.left = leftSpace;
-  document.getElementById("clk_shp_dig").style.left = leftSpace;
+  analogClock.style.left = document.getElementById("digital-clock").style.left =
+    leftSpace;
 }
 function setDigitalTime() {
   let meri; //a.m. or p.m.
   let t = " ";
-  let curr_time = new Date();
-  if (curr_time.getHours() == 0) {
-    //12A.M.
+  let currentDateTime = new Date();
+  if (currentDateTime.getHours() == 0) {
     t = 12;
     meri = "A.M.";
-    let note = document.getElementById("information-note-div");
-    note.style.color = "rgb(205,205,102)";
-    let back = document.getElementById("clk_shp_dig");
-    back.style.backgroundImage =
-      "linear-gradient(to right,rgb(164, 187, 215),rgb(41, 102, 234)";
-    document.body.style.backgroundColor = "rgb(75,75,75)";
-    curr_date.style.color = "rgb(250,250,239)";
-  } else if (curr_time.getHours() > 12) {
-    //1P.M to 11P.M
-    t = t + curr_time.getHours() - 12;
+  } else if (currentDateTime.getHours() > 12) {
+    t = t + currentDateTime.getHours() - 12;
     meri = "P.M.";
-    if (curr_time.getHours() >= 21) {
-      let note = document.getElementById("information-note-div");
-      note.style.color = "rgb(205,205,102)";
-      let back = document.getElementById("clk_shp_dig");
-      back.style.backgroundImage =
-        "linear-gradient(to right,rgb(164, 187, 215),rgb(41, 102, 234)";
-      document.body.style.backgroundColor = "rgb(75,75,75)";
-      curr_date.style.color = "rgb(250,250,239)";
-    }
-    if (curr_time.getHours() < 22) t = "0" + t;
-  } else if (curr_time.getHours() == 12) {
-    //12P.M.
-    t = t + curr_time.getHours();
+    if (currentDateTime.getHours() < 22) t = "0" + t;
+  } else if (currentDateTime.getHours() == 12) {
+    t = t + currentDateTime.getHours();
     meri = "P.M.";
-  } //1A.M. to 11A.m.
-  else {
-    t = curr_time.getHours();
+  } else {
+    t = currentDateTime.getHours();
     meri = "A.M.";
-    if (curr_time.getHours() < 10) t = "0" + t;
+    if (currentDateTime.getHours() < 10) t = "0" + t;
   }
-  if (curr_time.getMinutes() < 10) t = t + ":0" + curr_time.getMinutes();
-  else t = t + ":" + curr_time.getMinutes();
-  if (curr_time.getSeconds() < 10) t = t + ":0" + curr_time.getSeconds();
-  else t = t + ":" + curr_time.getSeconds() + " ";
+  if (currentDateTime.getMinutes() < 10)
+    t = t + ":0" + currentDateTime.getMinutes();
+  else t = t + ":" + currentDateTime.getMinutes();
+  if (currentDateTime.getSeconds() < 10)
+    t = t + ":0" + currentDateTime.getSeconds();
+  else t = t + ":" + currentDateTime.getSeconds() + " ";
   AMOrPM.innerText = meri;
   analogTime.innerText = t;
-  setDate(curr_time);
-  setBackgroundColor(curr_time);
+  setDate(currentDateTime);
+  setBackgroundColor(currentDateTime);
 }
 //Now for analog clock
 function setAnalogTime() {
-  var curr_time = new Date();
-  secondsNeedle.style.visibility = "visible";
-  minutesNeedle.style.visibility = "visible";
-  hoursNeedle.style.visibility = "visible";
-  sdeg = curr_time.getSeconds() * 6 - 90; //-90 has been done to convert the horizontal div into vertical div in the starting position(12:00:00 A.M.)
-  mdeg = curr_time.getMinutes() * 6 - 90 + curr_time.getSeconds() * 0.1;
-  hrs = curr_time.getHours() * 30 - 90 + curr_time.getMinutes() * 0.5;
+  var currentDateTime = new Date();
+  analogClock.style.display = "flex";
+  sdeg = currentDateTime.getSeconds() * 6 - 90; //-90 has been done to convert the horizontal div into vertical div in the starting position(12:00:00 A.M.)
+  mdeg =
+    currentDateTime.getMinutes() * 6 - 90 + currentDateTime.getSeconds() * 0.1;
+  hrs =
+    currentDateTime.getHours() * 30 - 90 + currentDateTime.getMinutes() * 0.5;
   if (window.innerWidth > 850) {
     secondsNeedle.style.transform = `translate(4vw) rotate(${sdeg}deg)`;
     minutesNeedle.style.transform = `translate(4vw) rotate(${mdeg}deg)`;
@@ -116,41 +97,40 @@ function setAnalogTime() {
     minutesNeedle.style.transform = `translate(34px) rotate(${mdeg}deg)`;
     hoursNeedle.style.transform = `translate(25.5px) rotate(${hrs}deg)`;
   }
-  setDate(curr_time);
-  setBackgroundColor(curr_time);
+  setDate(currentDateTime);
+  setBackgroundColor(currentDateTime);
 }
-function setDate(curr_time) {
-  if (curr_time.getDate() < 10)
-    curr_date.innerText = "0" + curr_time.getDate() + "/";
-  else curr_date.innerText = curr_time.getDate() + "/";
-  if (curr_time.getMonth() < 9)
-    curr_date.innerText += "0" + (curr_time.getMonth() + 1) + "/";
-  else curr_date.innerText += curr_time.getMonth() + 1 + "/";
-  curr_date.innerText += curr_time.getFullYear();
+function setDate(currentDateTime) {
+  if (currentDateTime.getDate() < 10)
+    dateElement.innerText = "0" + currentDateTime.getDate() + "/";
+  else dateElement.innerText = currentDateTime.getDate() + "/";
+  if (currentDateTime.getMonth() < 9)
+    dateElement.innerText += "0" + (currentDateTime.getMonth() + 1) + "/";
+  else dateElement.innerText += currentDateTime.getMonth() + 1 + "/";
+  dateElement.innerText += currentDateTime.getFullYear();
 }
-function setBackgroundColor(curr_time) {
+function setBackgroundColor(currentDateTime) {
   let note = document.getElementById("information-note-div");
-  if (curr_time.getHours() < 5 || curr_time.getHours() >= 21) {
+  let clocks = document.getElementsByClassName("clocks");
+  if (currentDateTime.getHours() < 5 || currentDateTime.getHours() >= 21) {
     note.style.color = "rgb(205,205,102)";
-    let back = document.getElementsByClassName("clk_shp");
-    back[0].style.backgroundImage =
+    clocks[0].style.backgroundImage =
       "linear-gradient(to right,rgb(164, 187, 215),rgb(41, 102, 234)";
-    back[1].style.backgroundImage =
+    clocks[1].style.backgroundImage =
       "linear-gradient(to right,rgb(164, 187, 215),rgb(41, 102, 234)";
     document.body.style.backgroundColor = "rgb(60,60,60)";
-    curr_date.style.color = "rgb(250,250,239)";
+    dateElement.style.color = "rgb(250,250,239)";
     document.getElementById("choice").style.color = document.getElementById(
       "color-selection-div"
     ).style.color = "rgb(250,250,239)";
   } else {
     note.style.color = "black";
-    let back = document.getElementsByClassName("clk_shp");
-    back[0].style.backgroundImage =
+    clocks[0].style.backgroundImage =
       "linear-gradient(to right,rgb(255, 111, 0),rgb(221, 255, 0))";
-    back[1].style.backgroundImage =
+    clocks[1].style.backgroundImage =
       "linear-gradient(to right,rgb(255, 111, 0),rgb(221, 255, 0))";
     document.body.style.backgroundColor = "rgb(250,250,255)";
-    curr_date.style.color = "black";
+    dateElement.style.color = "black";
     document.getElementById("choice").style.color = document.getElementById(
       "color-selection-div"
     ).style.color = "black";
@@ -164,10 +144,11 @@ function timeColorChange(value) {
   }
   document.querySelector('input[type = "color"]').style.borderColor = value;
 }
-function clockColor(event) {
+function changeClockColor(event) {
   timeColorChange(event.target.value);
 }
 function resetColor() {
   timeColorChange("#000000");
   document.querySelector('input[type = "color"]').value = "#000000";
 }
+digclk.checked = true;
